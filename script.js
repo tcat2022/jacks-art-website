64a3959cd63eb0a0c2e1b30f0da1ee9770ac73dd
tommy <tcatty7@gmail.com>
2023-07-18 13:11:15 -0400

    more changes to home page: navbar and image size

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function parallax() {
     var s = document.getElementById("scroller");
     var t = document.getElementById("img");
   var yPos = 0 + window.pageYOffset/4;  
-  var yPos1 = 0 + window.pageYOffset/8;  
+  var yPos1 = 0 + window.pageYOffset/6;  
   s.style.top = 50 + yPos + "%"; 
   t.style.top = 0 + yPos1 + "%";
 
