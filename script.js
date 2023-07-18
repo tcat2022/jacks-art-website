56b5240e4a2349390339b93d65b32571e8e5b704
tommy <tcatty7@gmail.com>
2023-07-17 20:53:08 -0400

    first commit yay!!!

---
+++ script.js
@@ -0,0 +1,15 @@
+function parallax() {
+    var s = document.getElementById("scroller");
+    var t = document.getElementById("img");
+  var yPos = 0 + window.pageYOffset/4;  
+  var yPos1 = 0 + window.pageYOffset/8;  
+  s.style.top = 50 + yPos + "%"; 
+  t.style.top = 0 + yPos1 + "%";
+
+  console.log(t)
+}
+
+window.addEventListener("scroll", function(){
+    parallax(); 
+
+});
